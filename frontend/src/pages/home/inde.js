b6c272f22e861ecbe441caabb74b7a151f075249
shivprajapat <shivkumarprajapat232@gmail.com>
2023-01-13 18:42:49 +0530

    frontend navbar ui

--- frontend/src/pages/home/inde.js
+++ frontend/src/pages/home/inde.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar';
 import './style.scss';
 
 const Home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+      <Navbar/>
+    </div>
   )
 }
 
