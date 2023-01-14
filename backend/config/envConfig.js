21550200c9a29069e586991861ebb997d09a2b6c
shivprajapat <shivkumarprajapat232@gmail.com>
2023-01-14 17:55:27 +0530

    crypto-js package use password hash added

--- backend/config/envConfig.js
+++ backend/config/envConfig.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 module.exports = {
     PORT: process.env.PORT,
-    MONGO_URL: process.env.MONGO_URL
+    MONGO_URL: process.env.MONGO_URL,
+    SECRET_KEY: process.env.SECRET_KEY
 };
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const env = require("../config/envConfig");
 
 // @desc    Set Movie
 // @route   POST /api/auth
@@ -7,7 +9,10 @@ const getRegister = async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            env.SECRET_KEY
+        ).toString()
     })
     console.log('newUser :>> ', newUser);
     try {
