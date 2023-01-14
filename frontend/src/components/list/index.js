4d7b1fa03bae5dce9f0aee09b81d1fb0ea86e0e2
shivprajapat <shivkumarprajapat232@gmail.com>
2023-01-14 10:42:47 +0530

    frontend list slider add without library

--- frontend/src/components/list/index.js
+++ frontend/src/components/list/index.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './style.scss';
 import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 import ListItem from '../listitem';
 
 const List = () => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <MdArrowBackIos className="sliderArrow left" />
-        <div className="container">
+        <MdArrowBackIos
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
           <ListItem index={0} />
           <ListItem index={1} />
           <ListItem index={2} />
@@ -21,7 +42,10 @@ const List = () => {
           <ListItem index={8} />
           <ListItem index={9} />
         </div>
-        <MdArrowForwardIos className="sliderArrow right" />
+        <MdArrowForwardIos
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
       </div>
     </div>
   )
