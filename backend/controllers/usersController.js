2b616360f8ef25e2b5d196da991fa44803753219
shivprajapat <shivkumarprajapat232@gmail.com>
2023-02-24 19:14:23 +0530

    backend All User api

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -6,7 +6,17 @@ const User = require("../models/User");
 // @route   GET /api/v1/users
 // @access  Public
 const getUser = async (req, res) => {
-    res.send({ message: "get user" })
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find().sort({ _id: -1 }).limit(10) : await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed to see all users!");
+    }
 }
 
 // @desc    Get Find User
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
-const { getUser, getStats, getUserID, updateUser, deleteUser, } = require("../controllers/usersController");
+const {
+  getUser,
+  getStats,
+  getUserID,
+  updateUser,
+  deleteUser,
+} = require("../controllers/usersController");
 const verify = require("../middleware/authMiddleware");
 
 // ! Users
 
-router.get('/', verify, getUser);
-router.get('/stats', getStats);
-router.get('/find/:id', getUserID);
-router.route('/:id').put(verify, updateUser).delete(verify, deleteUser)
+router.get("/", verify, getUser);
+router.get("/stats", getStats);
+router.get("/find/:id", getUserID);
+router.route("/:id").put(verify, updateUser).delete(verify, deleteUser);
 
 module.exports = router;
