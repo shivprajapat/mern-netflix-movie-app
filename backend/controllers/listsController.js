2637a02a3419baae24ff5e1ce6843acf7f10a4cc
shivprajapat <shivkumarprajapat232@gmail.com>
2023-02-25 12:11:30 +0530

    backend Delete List Api

--- backend/controllers/listsController.js
+++ backend/controllers/listsController.js
@@ -15,7 +15,7 @@ const setList = async (req, res) => {
         const newList = new List(req.body);
         try {
             const saveList = await newList.save()
-            res.status(200).json(saveList);
+            res.status(201).json(saveList);
         } catch (error) {
             res.status(500).json(error)
         }
@@ -28,7 +28,16 @@ const setList = async (req, res) => {
 // @access  Private
 
 const deleteList = async (req, res) => {
-    res.status(200).json("delete List request!");
+    if(req.user.isAdmin){
+        await List.findByIdAndDelete(req.params.id)
+        try {
+            res.status(201).json('The list has been delete...');
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }else{
+        res.status(403).json("You are not allowed!");
+    }
 }
 
 module.exports = {
